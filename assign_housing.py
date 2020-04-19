a4e7dafe8afe2bc410920095a605589d6eabb0bf
dsmolyak <dsmolyak@gmail.com>
2020-04-19 13:40:17 -0400

    refactored stat generation

--- assign_housing.py
+++ assign_housing.py
@@ -1,8 +1,22 @@
 import pandas as pd
 import random
+import time
 
 
-def assign(race_dist, applicant_df, housing_df):
+def compile_stats(applicant_df, housing_df, total_assignments, race_assignments, disability_assignments):
+    print('%d/%d, %f' % (total_assignments, len(housing_df), total_assignments / len(housing_df) * 100))
+    print(race_assignments)
+    race_percents = {}
+    for race, num in race_assignments.items():
+        race_percents[race] = num / len(applicant_df[applicant_df['Race'] == race])
+    print(race_percents)
+    num_dis_applicants = len(applicant_df[applicant_df['Disability'] == 'Yes'])
+    print('%d/%d, %f' % (disability_assignments, num_dis_applicants, disability_assignments / num_dis_applicants * 100))
+    print(disability_assignments)
+
+
+def assign_slow(race_dist, applicant_df, housing_df):
+    start = time.time()
     zips = set(housing_df['Zip Code'])
     race_limit_map = {} # find restrictions on how many of each race can be in a zip (proxy for block for now)
     for zip in zips:
@@ -15,6 +29,7 @@ def assign(race_dist, applicant_df, housing_df):
         disability_limit_map[zip] = len(housing_df[(housing_df['Zip Code'] == zip) &
                                                    (housing_df['Disability Friendly'] == 'Yes')])
 
+    print('middle', time.time() - start)
     total_assignments = 0
     race_assignments = {}
     disability_assignments = 0
@@ -35,9 +50,8 @@ def assign(race_dist, applicant_df, housing_df):
             race_assignments[race] += 1
             total_assignments += 1
 
-    print(total_assignments)
-    print(race_assignments)
-    print(disability_assignments)
+    print('end', time.time() - start)
+    compile_stats(applicant_df, housing_df, total_assignments, race_assignments, disability_assignments)
 
 
 if __name__ == '__main__':
