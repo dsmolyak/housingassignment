806ac6000a49253e165dce5bde1268bc569e9f6d
PrestonTong <preston.k.tong@gmail.com>
2020-05-13 18:18:47 -0400

    more than one utility to graph

--- generate_graphs.py
+++ generate_graphs.py
@@ -2,23 +2,28 @@ import matplotlib.pyplot as plt
 import csv
 import pandas as pd
 import os
+import numpy as np
 
 results_folder = "results"
 graph_folder = "graphs"
 
 x = []
-y = []
-
+disability_utility = []
+distance_utility = []
+width = 0.3
 
 for file in os.listdir(results_folder):
     results_df = pd.read_csv(results_folder + "/" + file)
 
     x.append(file)
-    y.append(results_df['Disability-Utility'].mean())
+    disability_utility.append(results_df['Disability-Utility'].mean())
+    distance_utility.append(results_df['Distance-Utility'].mean())
 
-plt.bar(x,y, label='Disability Utility')
+plt.bar(np.arange(len(x)),disability_utility, label='Disability Utility',width=width)
+plt.bar(np.arange(len(x)) + width,distance_utility, label='Distance Utility',width=width)
 plt.xlabel('Experiment')
-plt.ylabel('Disability Utility')
-plt.title('Disability Utility')
+plt.xticks(np.arange(len(x)),x)
+plt.ylabel('Average Utility')
+plt.title('Utility')
 plt.legend()
-plt.savefig(graph_folder + "/" + "disability-utility.png")
+plt.savefig(graph_folder + "/" + "utility.png")
