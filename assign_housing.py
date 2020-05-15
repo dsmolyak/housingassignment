dbdbc7b126740714ff39f4a0c36417cdbd72b6f3
dsmolyak <dsmolyak@gmail.com>
2020-05-15 19:05:53 -0400

    refactored race limits and utility calculations

--- assign_housing.py
+++ assign_housing.py
@@ -15,42 +15,91 @@ def compile_stats(applicant_df, housing_df, total_assignments, race_assignments,
     print(disability_assignments)
 
 
-def assign_lottery(race_dist, applicant_df, housing_df):
-    start = time.time()
+def find_race_limits(race_dist, housing_df):
     zips = set(housing_df['Zip Code'])
-    race_limit_map = {} # find restrictions on how many of each race can be in a zip (proxy for block for now)
+    race_limit_map = {}
     for zip in zips:
         race_limit_map[zip] = {}
+        total = len(housing_df[housing_df['Zip Code'] == zip])
         for race, percent in race_dist.items():
-            race_limit_map[zip][race] = percent / 100 * len(housing_df[housing_df['Zip Code'] == zip])
+            limit = round(percent / 100 * total) + 1
+            race_limit_map[zip][race] = limit
+    return race_limit_map
 
-    disability_limit_map = {} # find restrictions on how many people with disabilities can be in a zip
-    for zip in zips:
-        disability_limit_map[zip] = len(housing_df[(housing_df['Zip Code'] == zip) &
+
+def calc_distance_utilities(applicant_df, housing_df, location_matrix, variance):
+    distance_utilities = []
+    applicant_zips = list(applicant_df['Zip Code'])
+    housing_zips = list(housing_df['Zip Code'])
+    for i in range(0, len(applicant_df)):
+        samples = []
+        for j in range(0, len(housing_df)):
+            distance = location_matrix[applicant_zips[i]][housing_zips[j]]
+            distance = 1.0 if distance == 0 else distance
+            sample = max(0, np.random.normal(1.0 / distance, variance))
+            samples.append(sample)
+        norm_utils = [float(i) / sum(samples) for i in samples]
+        distance_utilities.append(norm_utils)
+
+    return distance_utilities
+
+
+def calc_disability_utilities(applicant_df, housing_df):
+    disability_utilities = []
+    disability_list = list(applicant_df['Disability'])
+    dis_fr_list = list(housing_df['Disability Friendly'])
+    num_matches = len(housing_df[housing_df['Disability Friendly'] == 'Yes']) * \
+                  len(applicant_df[applicant_df['Disability'] == 'Yes'])
+    f_samples = np.random.f(5, 10, num_matches)
+    count = 0
+    for i in range(0, len(applicant_df)):
+        utils = []
+        for j in range(0, len(housing_df)):
+            if disability_list[i] == 'Yes' and dis_fr_list[j] == 'Yes':
+                utils.append(f_samples[count] + 1)
+                count += 1
+            else:
+                utils.append(1)
+        disability_utilities.append(utils)
+
+    return disability_utilities
+
+
+def assign_lottery(applicant_df, housing_df, location_matrix, race_dist, disability):
+    zips = set(housing_df['Zip Code'])
+    race_limit_map = find_race_limits(race_dist, housing_df)
+
+    disability_limit_map = {}  # find restrictions on how many people with disabilities can be in a zip
+    for zip_code in zips:
+        disability_limit_map[zip_code] = len(housing_df[(housing_df['Zip Code'] == zip_code) &
                                                    (housing_df['Disability Friendly'] == 'Yes')])
 
-    print('middle', time.time() - start)
+    variance = 1
+    distance_utilities = calc_distance_utilities(applicant_df, housing_df, location_matrix, variance)
+    disability_utilities = calc_disability_utilities(applicant_df, housing_df)
+
+    # for i in range(0, len(applicant_df)):
+
     total_assignments = 0
     race_assignments = {}
     disability_assignments = 0
     for index, row in applicant_df.iterrows():
         race = row['Race']
         disability = row['Disability']
-        zip = row['Zip Code']
+        zip_code = row['Zip Code']
 
-        if race_limit_map[zip][race] > 0:
-            if disability == 'Yes' and disability_limit_map[zip] > 0:
-                disability_limit_map[zip] -= 1
+        if race_limit_map[zip_code][race] > 0:
+            if disability == 'Yes' and disability_limit_map[zip_code] > 0:
+                disability_limit_map[zip_code] -= 1
                 disability_assignments += 1
             elif disability == 'Yes':
                 continue
-            race_limit_map[zip][race] -= 1
+            race_limit_map[zip_code][race] -= 1
             if race not in race_assignments:
                 race_assignments[race] = 0
             race_assignments[race] += 1
             total_assignments += 1
 
-    print('end', time.time() - start)
     compile_stats(applicant_df, housing_df, total_assignments, race_assignments, disability_assignments)
 
 
@@ -157,13 +206,7 @@ def assign_optimal_by_unit(applicant_df, housing_df, location_matrix, race_dist,
     if race_dist:
         # find restrictions on how many of each race can be in a zip
         zips = list(set(housing_df['Zip Code']))
-        race_limit_map = {}
-        for zip in zips:
-            race_limit_map[zip] = {}
-            total = len(housing_df[housing_df['Zip Code'] == zip])
-            for race, percent in race_dist.items():
-                limit = round(percent / 100 * total) + 1
-                race_limit_map[zip][race] = limit
+        race_limit_map = find_race_limits(race_dist, housing_df)
 
         zip_indices = {}
         for index, row in housing_df.iterrows():
@@ -184,56 +227,28 @@ def assign_optimal_by_unit(applicant_df, housing_df, location_matrix, race_dist,
         if row['Disability'] == 'Yes':
             disability_indices.append(index)
 
+    # Constraints on disability friendly housing (must be matched to those with disabilities)
+    dis_fr_indices = []
+    for index, row in housing_df.iterrows():
+        if row['Disability Friendly'] == 'Yes':
+            dis_fr_indices.append(index)
+
     if disability:
         constraint = solver.RowConstraint(len(disability_indices), len(disability_indices), '')
         for j in range(0, m):
             for i in disability_indices:
                 constraint.SetCoefficient(x[i][j], 1)
 
-        # Constraints on disability friendly housing (must be matched to those with disabilities)
-        dis_fr_indices = []
-        for index, row in housing_df.iterrows():
-            if row['Disability Friendly'] == 'Yes':
-                dis_fr_indices.append(index)
         constraint = solver.RowConstraint(len(dis_fr_indices), len(dis_fr_indices), '')
         for j in dis_fr_indices:
             for i in disability_indices:
                 constraint.SetCoefficient(x[i][j], 1)
+
     # Set objective (create utility matrix)
     variance = 1
-    distance_utilities = []
-    applicant_zips = list(applicant_df['Zip Code'])
-    housing_zips = list(housing_df['Zip Code'])
-    for i in range(0, n):
-        samples = []
-        for j in range(0, m):  # This loop takes ~20 seconds due to pandas
-            distance = location_matrix[applicant_zips[i]][housing_zips[j]]
-            distance = 1.0 if distance == 0 else distance
-            sample = max(0, np.random.normal(1.0 / distance, variance))
-            samples.append(sample)
-        norm_utils = [float(i) / sum(samples) for i in samples]
-        distance_utilities.append(norm_utils)
-
-    disability_utilities = []
-    disability_list = list(applicant_df['Disability'])
-    dis_fr_list = list(housing_df['Disability Friendly'])
-    num_matches = len(housing_df[housing_df['Disability Friendly'] == 'Yes']) * \
-                  len(applicant_df[applicant_df['Disability'] == 'Yes'])
-    f_samples = np.random.f(5, 10, num_matches)
-    count = 0
-    for i in range(0, n):
-        utils = []
-        for j in range(0, m):
-            if disability_list[i] == 'Yes' and dis_fr_list[j] == 'Yes':
-                utils.append(f_samples[count] + 1)
-                count += 1
-            else:
-                utils.append(1)
-        disability_utilities.append(utils)
-
-    disability_utilities = np.asarray(disability_utilities)
-    distance_utilities = np.asarray(distance_utilities)
-    final_utilities = disability_utilities * distance_utilities
+    distance_utilities = calc_distance_utilities(applicant_df, housing_df, location_matrix, variance)
+    disability_utilities = calc_disability_utilities(applicant_df, housing_df)
+    final_utilities = np.asarray(disability_utilities) * np.asarray(distance_utilities)
 
     objective = solver.Objective()
     for i, row_a in applicant_df.iterrows():
@@ -264,7 +279,6 @@ def assign_optimal_by_unit(applicant_df, housing_df, location_matrix, race_dist,
 
                     disability_utility += disability_utilities[i][j]
                     distance_utility += distance_utilities[i][j]
-                    
 
         output_list = [total_count, len(housing_df), disability_totals, disability_utility, distance_utility]
 
